0218e75803a3dd1d36204b22c2b6ad85f47322cb
LuizFernandoWolffdeSouza <wolfluid@gmail.com>
2023-11-17 17:49:29 -0300

    exerciciosPHP(1)

---
+++ Luiz Wolff/primeiroarquivo.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        <input type="number" id ="num1" placeholder ="digite aqui">
+        <br>
+        <input type="number" id="num2" placeholder ="digite aqui">
+        <button onclick="somar()">Somar</button>
+    </form>
+
+    <script>
+        function somar(){
+            var num1 = document.getElementById('num1').value;
+            var num2 = document.getElementById('num2').value;
+           window.open(`segundoarquivo.php?numero1=${num1}&numero2=${num2}`)
+            alert(num1)
+            
+        }
+        
+
+    </script>
+ 
+
+
+</body>
+</html>
---
+++ Luiz Wolff/segundoarquivo.php
@@ -0,0 +1,14 @@
+<?php
+$numero1 = $_GET['numero1'];
+$numero2 = $_GET['numero2'];
+
+$soma = $numero1+ $numero2;
+echo " a soma dos numeros é: $soma"
+?>
+<br><br>
+
+
+<?php
+
+echo ' com o uso das aspas simples é literal então não registra a variável'
+?>
