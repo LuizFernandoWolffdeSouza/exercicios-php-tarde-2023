c024073d8d9210edaca230e8da6ea30979f32316
LuizFernandoWolffdeSouza <wolfluid@gmail.com>
2023-12-01 17:52:01 -0300

    exercicio

---
+++ Luiz Wolff/exerciciograus.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <label for="">Opções:</label>
+    <select name="" id="">
+        <option value="valor1">1</option>
+        <option value="valor2">2</option>
+        <option value="valor3">3</option>
+    </select>
+
+    <button onclick="verifica()">Escolher número</button>
+</form>
+<script>
+    alert('deseja converter uma temperatura?')
+    function verifica() {
+        
+        
+    }
+</script>
+    
+</body>
+</html>
